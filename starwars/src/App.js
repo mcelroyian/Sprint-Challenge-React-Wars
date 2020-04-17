5982ec025771092c1fd2e2d218be3f6dfb9c8b63
Ian McElroy <mcelroyian@gmail.com>
2020-04-17 12:01:08 -0500

    mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,48 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import Character from './components/Character'
 import './App.css';
-
-const luke = {
-    "name": "Luke Skywalker",
-    "height": "172",
-    "mass": "77",
-    "hair_color": "blond",
-    "skin_color": "fair",
-    "eye_color": "blue",
-    "birth_year": "19BBY",
-    "gender": "male",
-    "homeworld": "https://swapi.py4e.com/api/planets/1/",
-    "films": [
-      "https://swapi.py4e.com/api/films/1/",
-      "https://swapi.py4e.com/api/films/2/",
-      "https://swapi.py4e.com/api/films/3/",
-      "https://swapi.py4e.com/api/films/6/",
-      "https://swapi.py4e.com/api/films/7/"
-    ],
-    "species": [
-      "https://swapi.py4e.com/api/species/1/"
-    ],
-    "vehicles": [
-      "https://swapi.py4e.com/api/vehicles/14/",
-      "https://swapi.py4e.com/api/vehicles/30/"
-    ],
-    "starships": [
-      "https://swapi.py4e.com/api/starships/12/",
-      "https://swapi.py4e.com/api/starships/22/"
-    ],
-    "created": "2014-12-09T13:50:51.644000Z",
-    "edited": "2014-12-20T21:17:56.891000Z",
-    "url": "https://swapi.py4e.com/api/people/1/"
-}
-
-
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-const [currentChar, setCurrentChar] = useState(luke)
+const [currentChar, setCurrentChar] = useState(null)
 const [characters, setCharacters] = useState(null)
+const [next, setNext] = useState(null)
+const [prev, setPrev] = useState(null)
+const people = 'https://swapi.py4e.com/api/people/';
+
+const getExtraData = (character) => {
+
+}
+
+useEffect(() => {
+  axios.get(people)
+    .then( res => {
+      setCharacters(res.data.results)
+      setNext(res.data.next)
+      debugger
+    })
+    .catch( err => {
+      debugger
+    })
+}, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -51,6 +36,11 @@ const [characters, setCharacters] = useState(null)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className="wrapper">
+    {
+      characters && characters.map(char => <Character key={char.created} char={char} />)
+    }
+    </div>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,3 +1,31 @@
 import React from 'react'
+import { Card, CardBody, CardTitle, CardSubtitle, ListGroup, ListGroupItem } from 'reactstrap';
+import CardContainer from './CardContainer'
 // Write your Character component here
 
+const Character = props => {
+    const { name, gender, height, mass, hair_color, skin_color, eye_color } = props.char
+    return (
+        <CardContainer>
+            <Card>
+                <CardBody>
+                    <CardTitle>{name}</CardTitle>
+                    <CardSubtitle>{gender}</CardSubtitle>
+                        <ListGroup>
+                            <ListGroupItem>Height: {height}cm</ListGroupItem>
+                            <ListGroupItem>Mass: {mass}kg</ListGroupItem>
+                            <ListGroupItem>Hair Color: {hair_color}</ListGroupItem>
+                            <ListGroupItem>Skin Color: {skin_color}</ListGroupItem>
+                            <ListGroupItem>Eye Color: {eye_color}</ListGroupItem>
+                            <ListGroupItem>Species: TBD</ListGroupItem>
+                            <ListGroupItem>HomeWorld: TBD</ListGroupItem>
+                        </ListGroup>
+                </CardBody>
+            </Card>
+        </CardContainer>
+
+    )
+
+}
+
+export default Character
