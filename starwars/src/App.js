9f4a6c937adbb72541be434680aebed0b30705fc
Ian McElroy <mcelroyian@gmail.com>
2020-04-17 12:59:09 -0500

    answers

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,27 +7,57 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-const [currentChar, setCurrentChar] = useState(null)
+const [currentChar, setCurrentChar] = useState({})
 const [characters, setCharacters] = useState(null)
 const [next, setNext] = useState(null)
 const [prev, setPrev] = useState(null)
-const people = 'https://swapi.py4e.com/api/people/';
+const [query, setQuery] = useState('https://swapi.py4e.com/api/people/')
+const [homeQuery, setHomeQuery] = useState(null)
+const [speciesQuery, setSpeciesQuery] = useState(null)
+
+
+//For each Character
+// make a new request for homeworld and species.
+// save to state. 
+// pass to Character component
 
 const getExtraData = (character) => {
 
 }
 
 useEffect(() => {
-  axios.get(people)
+  const getData = () => {
+    axios.get(query)
+    .then( res => {
+      setSpeciesQuery(res.data.results)
+    })
+  }
+  getData();
+}, [speciesQuery])
+
+useEffect(() => {
+  const getData = () => {
+    axios.get(query)
+    .then( res => {
+      setHomeQuery(res.data.results)
+    })
+  }
+  getData();
+}, [homeQuery])
+
+useEffect(() => {
+  const fetchData = () => {
+    axios.get(query)
     .then( res => {
       setCharacters(res.data.results)
       setNext(res.data.next)
-      debugger
     })
-    .catch( err => {
-      debugger
-    })
-}, [])
+  }
+  fetchData();
+  
+}, [query])
+
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
